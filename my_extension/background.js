343fe5e107bad24448d0bcc1351dc5493a18c42c
Luke Burns <lukemburns@gmail.com>
2024-09-06 10:55:41 -0700

    background.js changes

--- my_extension/background.js
+++ my_extension/background.js
@@ -1,35 +1,66 @@
+console.log('PDA loaded')
+chrome.storage.sync.set({ history: [] }) // reset
+
+// Track when a tab becomes active (i.e., user switches tabs)
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  chrome.tabs.get(activeInfo.tabId, function(tab) {
+    if (tab.url) {
+      updateHistory(tab, 'tabActivated')
+    }
+  })
+})
+
+// Track when the user changes window focus
+chrome.windows.onFocusChanged.addListener(function(windowId) {
+  if (windowId !== chrome.windows.WINDOW_ID_NONE) {
+    chrome.tabs.query({ active: true, windowId: windowId }, function(tabs) {
+      if (tabs.length > 0) {
+        const tab = tabs[0]
+        if (tab.url) {
+          updateHistory(tab, 'windowFocusChanged')
+        }
+      }
+    })
+  }
+})
+
+// Track when the user navigates to a new page
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  // console.log('Tab update event triggered. changeInfo: ', changeInfo);
-  if (changeInfo.status === 'complete') {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      var tab = tabs[0]; // there will be only one in this array
-      console.log('Tab URL: ', tab.url);
-      chrome.storage.sync.get(['visitedUrls'], function(result) {
-        var visitedUrls = result.visitedUrls ? result.visitedUrls : [];
-        visitedUrls = visitedUrls.filter(url => url !== null && !url.startsWith('chrome'));
-        console.log('Current visitedUrls: ', visitedUrls);
-
-        // Send the visitedUrls to our server
-        fetch('http://localhost:3000/visitedUrls', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({visitedUrls: visitedUrls})
-        });
-      });
-    });
+  if (changeInfo.status === 'complete' && tab.url) {
+    updateHistory(tab, 'tabUpdated')
   }
-  if (changeInfo.url !== undefined && !changeInfo.url.startsWith('chrome')) {
-    console.log('URL changed. New URL: ', changeInfo.url);
-    chrome.storage.sync.get(['visitedUrls'], function(result) {
-      var visitedUrls = result.visitedUrls ? result.visitedUrls : [];
-      visitedUrls.push(changeInfo.url);
-      chrome.storage.sync.set({visitedUrls: visitedUrls}, function() {
-        console.log('URL ' + changeInfo.url + ' added to visitedUrls.');
-      });
-    });
-  }
-});
-console.log('Background script for My Extension');
+})
 
+function updateHistory (tab, event) {
+  chrome.storage.sync.get(['history'], function(result) {
+    const history = result.history ? result.history : []
+    const prev = history[history.length-1]
+    let curr = {
+      event,
+      id: tab.id,
+      url: tab.url,
+      title: tab.title,
+      windowId: tab.windowId,
+      groupId: tab.groupId,
+      openerTabId: tab.openerTabId,
+      lastAccessed: tab.lastAccessed,
+      timestamp: Date.now()
+    }
+
+    // append to history if new event
+    if (!prev || prev.id !== curr.id || prev.url !== curr.url) {
+      history.push(curr)
+      chrome.storage.sync.set({ history }, function() {
+        console.log('URL ' + tab.url + ' added to history.')
+        console.log('History:', history)
+      })
+      fetch('http://localhost:3000/event', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(curr)
+      })
+    }
+  })
+}
