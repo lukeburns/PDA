e0dfee1f76d812c8420697a6e0f420244c926b66
Alexander Kurz <alexhkurz@gmail.com>
2024-09-08 18:46:51 -0700

    parse renamed to toJSON

--- organize-metadata/organize-metadata.js
+++ organize-metadata/organize-metadata.js
@@ -1,16 +1,14 @@
 const intermoduleDataDir = '../intermodule-data';
 const fs = require('fs');
 const path = require('path');
-const bibtexParse = require('bibtex-parse-js');
-
-console.log(bibtexParse);
+const bibtexParse = require('bibtex-parse-js'); // provides .toJSON method and .toBibtex method
 
 function convertBibToJson() {
   const bibPath = path.join(__dirname, `${intermoduleDataDir}/history.bib`);
   const jsonPath = path.join(__dirname, `${intermoduleDataDir}/metadata.json`);
 
   const bibContent = fs.readFileSync(bibPath, 'utf-8');
-  const parsed = bibtexParse.parse(bibContent);
+  const parsed = bibtexParse.toJSON(bibContent);
 
   const jsonContent = JSON.stringify(parsed, null, 2);
   fs.writeFileSync(jsonPath, jsonContent);
