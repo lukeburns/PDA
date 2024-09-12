9beeadc72439853a7c11c5fdb1eb99d762e4d6f6
Alexander Kurz <alexhkurz@gmail.com>
2024-09-12 07:19:27 -0700

    fs.readFileSync.mockReturnValue currently not working

--- organize-metadata/organize-metadata.test.js
+++ organize-metadata/organize-metadata.test.js
@@ -3,15 +3,27 @@ const path = require('path');
 const bibtexParse = require('bibtex-parse-js');
 const { convertBibToJson } = require('./organize-metadata.js');
 
-test('convertBibToJson function is defined', () => {
-  console.log('Running test: convertBibToJson function is defined');
-  expect(typeof convertBibToJson).toEqual('function');
-});
+jest.mock('fs');
 
-test('convertBibToJson creates metadata.json file', () => {
-  console.log('Running test: convertBibToJson creates metadata.json file');
-  fs.writeFileSync = jest.fn();
-  fs.readFileSync.mockReturnValueOnce('@article{expectedDummy,\n  title={expected dummy title},\n}\n@book{dummyBook,\n  title={dummy book title},\n}');
-  convertBibToJson();
-  expect(fs.writeFileSync).toHaveBeenCalled();
+describe('convertBibToJson', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+  });
+
+  test('convertBibToJson function is defined', () => {
+    console.log('Running test: convertBibToJson function is defined');
+    expect(typeof convertBibToJson).toEqual('function');
+  });
+  
+  test('convertBibToJson creates metadata.json file', () => {
+    console.log('Running test: convertBibToJson creates metadata.json file');
+    fs.writeFileSync = jest.fn();
+    fs.readFileSync.mockReturnValue('@article{expectedDummy,\n  title={expected dummy title},\n}\n@book{dummyBook,\n  title={dummy book   title},\n}');
+    convertBibToJson();
+    expect(fs.writeFileSync).toHaveBeenCalled();
+    // Additional debugging
+    const bibContent = fs.readFileSync.mock.results[0].value;
+    console.log("Mocked BibTeX content:", bibContent);
+    });
 });
