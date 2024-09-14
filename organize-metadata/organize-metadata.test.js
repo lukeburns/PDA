a960523ac3dfcdd45edec9f6aff62a8f354dbc3c
Alexander Kurz <alexhkurz@gmail.com>
2024-09-14 14:41:16 -0700

    kargs file

--- organize-metadata/organize-metadata.test.js
+++ organize-metadata/organize-metadata.test.js
@@ -1,30 +1,15 @@
-const fs = jest.createMockFromModule('fs');
+const fs = require('fs');
 const path = require('path');
-const bibtexParse = require('bibtex-parse-js');
 const { convertBibToJson } = require('./organize-metadata.js');
 
-jest.mock('fs');
-
-/*
 test('convertBibToJson function is defined', () => {
   console.log('Running test: convertBibToJson function is defined');
   expect(typeof convertBibToJson).toEqual('function');
 });
-*/
 
 test('convertBibToJson creates metadata.json file', () => {
   console.log('Running test: convertBibToJson creates metadata.json file');
-  fs.writeFileSync = jest.fn();
-  const mockBibContent = '@article{expectedDummy,\n  title={expected dummy title},\n}\n@book{dummyBook,\n title={dummy book   title},\n}';
-  fs.readFileSync = jest.fn((filePath, encoding) => {
-    if (filePath.endsWith('history.bib') && encoding === 'utf-8') {
-      return mockBibContent;
-    }
-    return '';
-  });
-  const bibPath = path.join(__dirname, 'data/history.bib');
-  const jsonPath = path.join(__dirname, 'data/metadata.json');
-  convertBibToJson(bibPath, jsonPath);
-  expect(fs.writeFileSync).toHaveBeenCalled();
+  convertBibToJson();
+  const jsonPath = path.join(__dirname, '../intermodule-data/metadata.json');
+  expect(fs.existsSync(jsonPath)).toBe(true);
 });
-
