80e77b3319042a7562bbabcaaedb811a02850f07
Alexander Kurz <alexhkurz@gmail.com>
2024-09-14 19:11:07 -0700

    delete content.js

--- content.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('mouseup', () => {
-  const selectedText = window.getSelection().toString().trim();
-  if (selectedText) {
-    chrome.runtime.sendMessage({ type: 'highlight', text: selectedText });
-  }
-});
